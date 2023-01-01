3102ca24b7ceffb8c427add1fcb5d1959e798a41
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-31 18:29:22 -0800

    Allow player to update board

--- script.js
+++ script.js
@@ -3,20 +3,43 @@ const squares = document.querySelectorAll('.squares');
 
 const gameBoard = (() => {
 	const gameBoardArray = [
-		['X', '0', 'X'],
-		['0', 'X', '0'],
-		['0', 'X', '0'],
+		['', '', ''],
+		['', '', ''],
+		['', '', ''],
 	];
 	return { gameBoardArray };
 })();
 
-// console.log(gameBoard.gameBoardArray[0]);
-
 function displayGameBoard() {
 	squares.forEach((e) => {
 		e.textContent = gameBoard.gameBoardArray[e.dataset.row][e.dataset.index];
-		e.value = e.textContent;
 	});
 }
 
+// gameBoard.gameBoardArray[0][1] = 1234;
+
 displayGameBoard();
+
+const players = (marker = 'X') => {
+	const updateGameBoard = ([Y, X]) => {
+		gameBoard.gameBoardArray[Y][X] = marker;
+		displayGameBoard();
+	};
+
+	const receiveSquareInfo = () => {
+		squares.forEach((e) =>
+			e.addEventListener('click', () => {
+				let [X, Y] = [e.dataset.index, e.dataset.row];
+				updateGameBoard([Y, X]);
+			})
+		);
+	};
+
+	return { receiveSquareInfo };
+	// const placeMarker = (X) => gameBoard.gameBoardArray;
+};
+
+const player1 = players('X');
+const player2 = players('0');
+player1.receiveSquareInfo();
+// player2.receiveSquareInfo();
