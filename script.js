32421ec50a2c544ce6992d4b89eb77e6c4650e51
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-31 17:28:36 -0800

    Display array contents to game board

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+/* eslint-disable no-unused-vars */
+const squares = document.querySelectorAll('.squares');
 
+const gameBoard = (() => {
+	const gameBoardArray = [
+		['X', '0', 'X'],
+		['0', 'X', '0'],
+		['0', 'X', '0'],
+	];
+	return { gameBoardArray };
+})();
+
+// console.log(gameBoard.gameBoardArray[0]);
+
+function displayGameBoard() {
+	squares.forEach((e) => {
+		e.textContent = gameBoard.gameBoardArray[e.dataset.row][e.dataset.index];
+		e.value = e.textContent;
+	});
+}
+
+displayGameBoard();
